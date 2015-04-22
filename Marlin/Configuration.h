c8a89ae811e4c51d9848efffb02c040ad8145610
Ira Abramov <ira.abramov@alcatel-lucent.com>
2015-04-22 14:31:29 +0300

    well, 250000 doesn't work, but it is the next best speed.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -33,7 +33,7 @@
 #define SERIAL_PORT 0
 
 // This determines the communication speed of the printer
-#define BAUDRATE 115200
+#define BAUDRATE 230400
 
 // This enables the serial port associated to the Bluetooth interface
 //#define BTENABLED              // Enable BT interface on AT90USB devices
